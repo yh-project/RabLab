29bad479c94ca3f45dc732c0cd51eb6370e9cfbe
Capoomaru <anycar8515@ynu.ac.kr>
2022-03-20 23:54:05 +0900

    03/20

---
+++ qstn_1.js
@@ -0,0 +1,14 @@
+var selection_list = [];
+for(i in Range(0,9)) {
+    selection_list.push(document.getElementById("qstn"+i));
+}
+
+for (i in Range(1,9)) {
+    selection_list[i].style.visibility="hidden";
+}
+
+var cur_state = 0;
+
+function input_items() {
+    if ( )
+}
