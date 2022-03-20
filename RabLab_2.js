fe8c1667255352a6773d042852a489a2d5f6fe25
yh-project <yoha6865@gmail.com>
2022-03-21 01:15:47 +0900

    220321

--- RabLab_2.js
+++ RabLab_2.js
@@ -4,17 +4,24 @@ var st2 = document.getElementById("step2");
 var txt3 = document.getElementById("text3");
 var st3 = document.getElementById("step3");
 var txt4 = document.getElementById("text4");
+var vid1 = document.getElementById("vid_1");
+var vid2 = document.getElementById("vid_2");
+var vid3 = document.getElementById("vid_3");
 
 st3.style.visibility = "hidden";
 txt4.style.visibility = "hidden";
+vid3.style.visibility = "hidden";
 
 function txt_change() {
     if(cur_state == 0) {
         st1.style.visibility = "hidden";
         st2.style.visibility = "hidden";
         txt3.style.visibility = "hidden";
+        vid1.style.visibility = "hidden";
+        vid2.style.visibility = "hidden";
         st3.style.visibility = "visible";
         txt4.style.visibility = "visible";
+        vid3.style.visibility = "visible";
         cur_state++;
     }
     else if(cur_state == 1) {
--- qstn_1.js
+++ qstn_1.js
@@ -1,4 +1,4 @@
-var cur_state = 0;
+/*var cur_state = 0;
 var q1 = document.getElementById("qstn1");
 var q2 = document.getElementById("qstn2");
 var q3 = document.getElementById("qstn3");
@@ -7,6 +7,49 @@ var q5 = document.getElementById("qstn5");
 var q6 = document.getElementById("qstn6");
 var q7 = document.getElementById("qstn7");
 
-function input_items() {
-    
+var input_arr = [q1, q2, q3, q4, q5, q6, q7];
+for(var i = 1; i<input_arr.length; i++) {
+    input_arr[i].style.visibility = "hidden";
 }
+
+var names = document.getElementById("names");
+
+function input_items() {
+        switch(cur_state) {
+            case 0:
+                change_visibility(cur_state);
+                names.style.visibility = "hidden";
+                cur_state++;
+                break;
+            case 1:
+                change_visibility(cur_state);
+                cur_state++;
+                break;
+            case 2:
+                change_visibility(cur_state);
+                cur_state++;
+                break;
+            case 3:
+                change_visibility(cur_state);
+                cur_state++;
+                break;
+            case 4:
+                change_visibility(cur_state);
+                cur_state++;
+                break;
+            case 5:
+                change_visibility(cur_state);
+                cur_state++;
+                break;
+            case 6:
+                change_visibility(cur_state);
+                break;
+        }
+}
+
+function change_visibility(idx) {
+    for(var i = 0; i<input_arr.length; i++) {
+        if(i == (idx+1)) input_arr[i].style.visibility = "visible";
+        else input_arr[i].style.visibility = "hidden";
+    }
+}*/
