a243e17aa6e737fec1d8ccfcfab49261b296700e
yh-project <yoha6865@gmail.com>
2022-03-20 15:44:50 +0900

    220320

--- RabLab_3.js
+++ RabLab_3.js
@@ -2,8 +2,12 @@ var cur_state = 0;
 var txt5 = document.getElementById("text5");
 var txt6 = document.getElementById("text6");
 var txt7 = document.getElementById("text7");
+var start_box = document.getElementById("start-box");
+var vid = document.getElementById("vid");
 txt6.style.visibility = "hidden";
 txt7.style.visibility = "hidden";
+start_box.style.visibility =  "hidden";
+vid.style.visibility = "hidden";
 
 function txt_change() {
     if(cur_state == 0) {
@@ -14,6 +18,12 @@ function txt_change() {
     else if(cur_state == 1) {
         txt6.style.visibility = "hidden";
         txt7.style.visibility = "visible";
+        start_box.style.visibility = "visible";
+        vid.style.visibility = "visible";
         cur_state++;
     }
+}
+
+function start_survey() {
+    
 }
