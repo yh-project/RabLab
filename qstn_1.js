897437a6c02ea4846a3a9a921d0174cc641c2902
yh-project <yoha6865@gmail.com>
2022-03-20 23:52:47 +0900

    220320

---
+++ qstn_1.js
@@ -0,0 +1,12 @@
+var cur_state = 0;
+var q1 = document.getElementById("qstn1");
+var q2 = document.getElementById("qstn2");
+var q3 = document.getElementById("qstn3");
+var q4 = document.getElementById("qstn4");
+var q5 = document.getElementById("qstn5");
+var q6 = document.getElementById("qstn6");
+var q7 = document.getElementById("qstn7");
+
+function input_items() {
+    
+}
