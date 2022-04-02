71ffcae4311747a3d527bf9eb6ac5f16341636ec
Capoomaru <anycar8515@ynu.ac.kr>
2022-04-02 14:56:51 +0900

    0402

--- qstn_1.js
+++ qstn_1.js
@@ -1,5 +1,70 @@
+var cur_state = 0;
+
+var pick_txt = [
+    [
+    '내 방 침대',
+    '자주가던 카페',
+    '산 꼭대기',
+    '과거 어느 순간',
+    '한적한 바다',
+    '넓은 들판',
+    '우주 어느 행성',
+    '여기가 어디일까요..'
+    ],
+    [
+    '화창한 봄 날씨',
+    '선선한 가을 날씨',
+    '안개 낀 새벽 날씨',
+    '비가 내리는 날씨',
+    '흠.. 잘 모르겠어요',
+    '푸른 여름 날씨',
+    '포근한 겨울 날씨',
+    '평소의 맑은 날씨',
+    '여기가 어디일까요..',
+    ],
+    [
+    '가족',
+    '친구',
+    '애인',
+    '반려동물',
+    '좋아하는 연예인',
+    '모르는 사람인데?',
+    ],
+    [
+    '취미 생활',
+    '게임',
+    '휴식',
+    '맛있는 거 먹기',
+    '잠',
+    '대화',
+    '술',
+    '잘 모르겠어요'
+    ],
+    [
+    '돈',
+    '누군가의 죽음',
+    '외로움',
+    '선택의 실수',
+    '인간관계',
+    '직장',
+    '건강'
+    ],
+    [
+    '행복 그 자체였어요',
+    '나름 만족해요',
+    '나쁘지 않았어요',
+    '아쉬움이 많이 남아요',
+    '너무 지치고 힘들었어요',
+    '아무 생각이 안들어요'
+    ]
+]
+for (let i =1;i<=6;i++) {
+    pick_txt.push('pick_txt'+i);
+}
+
 var pick_list = [];
 var qstn_list = [];
+
 var name_box = document.getElementById("names");
 for(let i = 1; i <= 9; i++) {
     pick_list.push(document.getElementById("p"+i));
@@ -17,18 +82,15 @@ document.getElementById("pick").style.visibility="hidden"
 
 var cur_state = 0;
 const items_cnt = [8,9,6,8,7,6];
-// const items_css = {}
-// items_css[6] = new sytle;
-// items_css[7] = new sytle;
-// items_css[8] = new sytle;
-// items_css[9] = new style;
+const items_css = [];
+items_css.push('./pick8_2.css');
+items_css.push('./pick9_3.css')
+items_css.push('./pick6_4.css')
+items_css.push('./pick8_5.css')
+items_css.push('./pick7_6.css')
+items_css.push('./pick6_7.css')
 
 var user_name;
-
-// var input_arr = [q1, q2, q3, q4, q5, q6, q7];
-// for(var i = 1; i<input_arr.length; i++) {
-//     input_arr[i].style.visibility = "hidden";
-// }
 
 var names = document.getElementById("names");
 
@@ -65,7 +127,31 @@ function back_change() {
     history.back();
 }
 
+
+
 function front_change() {
-    sessionStorage.removeItem('cur_state');
-    window.location.href = 'qstn_1.html';
+
+    cur_state++;
+
+    document.getElementById('names').style.visibility='hidden';
+    if(cur_state>1) {
+        document.getElementById('pick_css').href=items_css[cur_state-1];
+    }
+    
+    for(let i=1;i<=items_cnt[cur_state-1];i++) {
+        document.getElementById('p'+i).textContent = pick_txt[cur_state-1][i-1];
+        document.getElementById('p'+i).style.visibility="visible";
+    }
+    for(let i = items_cnt[cur_state-1]+1;i<=9;i++) {
+        document.getElementById('p'+i).style.visibility="hidden";
+     }
+    
+    document.getElementById("qstn"+cur_state).style.visibility="hidden";
+    document.getElementById("qstn"+(cur_state+1)).style.visibility="visible";
+
 }
+
+
+
+//sessionStorge : 리스트형식? -> 중복선택이 있음ㅋㅋㅋㅋㅋㅋㅋ => 중복선택을 2자리 숫자로 표시?
+//선택된 애만 테두리 보이도록하기
